b697c24b44f4e48ab3f3a4ec3b0d16dd0cbcd953
Simon Whitaker <swhitaker@cpan.org>
2008-02-14 15:37:28 +0000

    Import of SWHITAKER/Amazon-SQS-Simple-0.7 from CPAN.
    gitpan-cpan-distribution: Amazon-SQS-Simple
    gitpan-cpan-version:      0.7
    gitpan-cpan-path:         SWHITAKER/Amazon-SQS-Simple-0.7.tar.gz
    gitpan-cpan-author:       SWHITAKER
    gitpan-cpan-maturity:     released

--- lib/Amazon/SQS/Simple.pm
+++ lib/Amazon/SQS/Simple.pm
@@ -5,7 +5,7 @@ use Amazon::SQS::Simple::Queue;
 
 use base qw(Exporter Amazon::SQS::Simple::Base);
 
-our $VERSION   = '0.6';
+our $VERSION   = '0.7';
 our @EXPORT_OK = qw( timestamp );
 
 sub GetQueue {
@@ -88,7 +88,7 @@ Service
     print $msg->MessageBody() # Hello world!
 
     # Delete the message
-    $q->DeleteMessage($msg->MessageId());
+    $q->DeleteMessage($msg->ReceiptHandle());
 
     # Delete the queue
     $q->Delete();
